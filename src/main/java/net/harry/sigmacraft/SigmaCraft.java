4311afb5eba7f1891a17ddbf7b25bd35bedeb66f
Notion86 <Harrisontysoe@gmail.com>
2024-09-04 15:23:00 +1000

    Initial commit

---
+++ src/main/java/net/harry/sigmacraft/SigmaCraft.java
@@ -0,0 +1,18 @@
+package net.harry.sigmacraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SigmaCraft implements ModInitializer {
+	public static final String MOD_ID = "sigmacraft";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
